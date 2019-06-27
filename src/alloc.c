cb76e966941328bf7242ce9939ebe986555de2bf
Danila Kutenin <danlark@sandbox-286092131>
2019-06-27 18:08:29 +0300

    remove overridings for mimalloc

--- src/alloc.c
+++ src/alloc.c
@@ -9,11 +9,11 @@ terms of the MIT license. A copy of the license can be found in the file
 #include "mimalloc-atomic.h"
 
 #include <string.h>  // memset
-
+/*
 #define MI_IN_ALLOC_C
 #include "alloc-override.c"
 #undef MI_IN_ALLOC_C
-
+*/
 // ------------------------------------------------------
 // Allocation
 // ------------------------------------------------------
